9b9c083791f306b3dffe4fa195f92c2332e9e9bd
santosh_pawar <psantosh@us.ibm.com>
2017-01-05 23:06:08 +0530

    Reading message body from resource

--- out.sh
+++ out.sh
@@ -14,11 +14,12 @@ smtp_host="$(jq -rcM '.source.smtp_host' < "${payload}")"
 smtp_port="$(jq -rcM '.source.smtp_port' < "${payload}")"
 smtp_username="$(jq -rcM '.source.smtp_username' < "${payload}")"
 smtp_password="$(jq -rcM '.source.smtp_password' < "${payload}")"
-recepient="$(jq -rcM '.source.recepient' < "${payload}")"
+recepient="$(jq -rcM '.source.default_recepient' < "${payload}")"
 input_dir="$(jq -rcM '.params.input_dir' < "${payload}")"
+email_body="$(jq -rcM '.params.email_body' < "${payload}")"
 
 # Check if all values are set
-if [[ -z $smtp_host ]] || [[ -z $smtp_port ]] || [[ -z $smtp_username ]] || [[ -z $smtp_password ]] || [[ -z $recepient ]] || [[ -z $input_dir ]]; then
+if [[ -z $smtp_host ]] || [[ -z $smtp_port ]] || [[ -z $smtp_username ]] || [[ -z $smtp_password ]] || [[ -z $recepient ]] || [[ -z $input_dir ]] || [[ -z $email_body ]]; then
 	echo "Missing parameters in resource. Make sure you have defined all required parameters on this resource."
 	exit 1
 fi
@@ -26,8 +27,8 @@ fi
 # Read subject file
 subject=$(cat ${1}/${input_dir}/subject)
 
-# Copy message body file
-cp -f ${1}/${input_dir}/body /tmp/
+# Create message body file
+echo $email_body > /tmp/body
 
 # Add Concourse CI build URL at the end of the email body
 EMAIL_BUILD_URL="${ATC_EXTERNAL_URL}/builds/${BUILD_ID}"
@@ -37,7 +38,7 @@ echo "Concourse build URL: $EMAIL_BUILD_URL" >> /tmp/body
 # Check if file named 'to' is available. If yes, then override the 'recepient' from source param
 if [ -f ${1}/${input_dir}/to ]; then
 	recepient=$(cat ${1}/${input_dir}/to)
-	echo "File 'to' found, overriding recepient to - $recepient"
+	echo "File 'to' found, overriding recepients to - $recepient"
 fi
 
 on_success="$(jq -rcM '.params.on_success // "false"' < "${payload}")"
